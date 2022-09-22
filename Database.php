35fe9835b8c2d001f32db97935c259d301ab3cc5
Ivan Fuentes <consultant-ifuentes@remine.com>
2022-09-21 20:07:11 -0600

    Database class added

---
+++ Database.php
@@ -0,0 +1,40 @@
+<?php 
+
+class Database {
+    static private $pdo;
+    static public $operators = ['=', '<>', 'and', 'or', 'like'];
+    private $table;
+
+    public static function connect(string $method) {
+        static::$pdo = $method;
+        return new static;
+    }
+
+    public function table (string $name) {
+        $this->table = $name;
+        return $this;
+    }
+
+    public function insert(array $data) {
+        var_dump('Connected to the database using' . static::$pdo);
+        var_dump("INSERTING INTO { $this->table } VALUES(".json_encode($data).")");
+    }
+
+    public static function create(array $data) {
+        var_dump('Creating a new database with ' . self::$pdo);
+    }
+
+    public function getMethod() {
+        return self::$pdo;
+    }
+}
+
+header('Content-Type: text/plain', true);
+var_dump(Database::$operators);
+Database::connect('pdo')->table('users')->insert(['name' => 'John Doe']);
+Database::create([]);
+
+$db = new Database;
+var_dump($db->getMethod());
+
+?>
